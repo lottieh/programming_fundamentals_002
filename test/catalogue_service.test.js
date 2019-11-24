8ece77752e68f788b1b5a1b94335066ab7af6871
Charlotte House <cdmhouse@sky.com>
2019-11-24 13:01:47 +0000

    More test conditions added to countBooksByKeyword Function

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -2,7 +2,7 @@ const catalogueService = require("../app/catalogue_service");
 
 describe("catalogueService", () => {
   describe("catalogueService.countBooks", () => {
-    test("returns the number of books in the list", () => {
+    test("Returns the number of books in the list", () => {
       expect(catalogueService.countBooks()).toBe(20);
     });
   });
@@ -10,11 +10,11 @@ describe("catalogueService", () => {
 
 describe("catalogueService", () => {
   describe("catalogueService.checkBook", () => {
-    test("returns true if the book exists in the list", () => {
+    test("Returns true if the book exists in the list", () => {
       expect(catalogueService.checkBook("The Catcher in the Rye by J.D. Salinger")).toBe(true);
 
     });
-    test("returns false if the book does not exists in the list", () => {
+    test("Returns false if the book does not exists in the list", () => {
       expect(catalogueService.checkBook("The BFG by Roald Dahl")).toBe(false);
     });
   })
@@ -22,14 +22,14 @@ describe("catalogueService", () => {
 
 describe("catalogueService", () => {
   describe('catalogueService.countBooksByFirstLetter', () => {
-    test('returns the number of books starting with a given letter', () => {
+    test('Returns the number of books starting with a given letter', () => {
       expect(catalogueService.countBooksByFirstLetter('B')).toBe(3);
     });
   })
 });
 describe("catalogueService", () => {
   describe('catalogueService.countBooksByFirstLetter', () => {
-    test('returns zerto if no books starting with a given letter', () => {
+    test('Returns zerto if no books starting with a given letter', () => {
       expect(catalogueService.countBooksByFirstLetter('X')).toBe(0);
     });
   })
@@ -37,10 +37,13 @@ describe("catalogueService", () => {
 
 describe('catalogueService', () => {
   describe('catalogueService.countBooksByKeyword', () => {
-    test('returns the number of books containing the specified word', () => {
+    test('Returns the number of books containing the specified keyword', () => {
       expect(catalogueService.countBooksByKeyword('Normal')).toBe(2);
       expect (catalogueService.countBooksByKeyword("With")).toBe(1); 
       expect (catalogueService.countBooksByKeyword("Benjamin")).toBe(0);
+      expect (catalogueService.countBooksByKeyword(2666)).toBe(1);
+      expect(catalogueService.countBooksByKeyword(true)).toBe(0);
+      expect(catalogueService.countBooksByKeyword([1, 2, 3])).toBe(0); 
     });
   })
 }); 
